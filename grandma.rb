33d15e0c8114c4d9821b00a776145beee186c1b0
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-24 00:21:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
     
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
